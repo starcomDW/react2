88ed7a8be8c0f7489b3f779892c27423d772a66f
Dean <deanw@starcomsoft.com>
2018-03-01 14:03:56 -0800

    two merge1

--- react1/src/main/java/react/basic/one/controller/HomeController.java
+++ react1/src/main/java/react/basic/one/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// two merge1
 @Controller
 public class HomeController {
  
