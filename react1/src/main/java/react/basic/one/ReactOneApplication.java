7f8bac79b54b949e299ab87b8cded933fe8d8dd3
Dean <deanw@starcomsoft.com>
2018-03-01 14:04:49 -0800

    four merge1

--- react1/src/main/java/react/basic/one/ReactOneApplication.java
+++ react1/src/main/java/react/basic/one/ReactOneApplication.java
@@ -3,6 +3,7 @@ package react.basic.one;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// four merge1
 @SpringBootApplication
 public class ReactOneApplication {
  
