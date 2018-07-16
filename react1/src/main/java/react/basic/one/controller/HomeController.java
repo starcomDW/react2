234b4e826e11aad2138eaff74f482d4973fcadfc
Dean <deanw@starcomsoft.com>
2018-07-16 14:16:57 -0700

    Testing new version of git

--- react1/src/main/java/react/basic/one/controller/HomeController.java
+++ react1/src/main/java/react/basic/one/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// two merge1
+// two merge1 test new version of git
 // another change on merge one that will not have a conflict
 @Controller
 public class HomeController {
